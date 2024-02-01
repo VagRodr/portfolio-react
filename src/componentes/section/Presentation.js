c646db24f5f53fbcaaffc53630911d9903c372d5
Vagner <vagnerlrg@gmail.com>
2024-02-01 11:23:00 -0300

    versão 2.0, aplicando efeito na apresentação e cards de habilidades

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,12 +1,44 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
-import ButtonB from '../elements/ButtonB'
+import {useEffect, useState} from 'react'
 
 function Presentation(){
+
+    const [text, setText] = useState('');
+    const toRotate = ['Vagner Rodrigues!', 'Desenvolvedor Front End!'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 200;
+    const [delta, setDelta] = useState(100);
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+    }, [text])
+
+    const toType = ()=>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length-1) : fullText.substring(0, text.length+1);
+
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText ===''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+    }
+
     return(
         <div className={styles.presentation} id='Presentation'>
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou Vagner Rodrigues!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
                 Profissional proativo, concentrado, com facilidade para trabalhar em equipe.<br/> 
                 Estou em transição de carreira para a área de tecnologia.<br/>
